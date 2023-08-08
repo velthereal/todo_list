1e429ad2f86a0240d855e60680aec0b19f66772d
velthereal <lera2005olex@gmail.com>
2023-08-08 19:43:15 +0300

    added js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function pageLoaded() {
-	let tipsButton = document.getElementById('tips');
-	let modalWindow = document.querySelector('.modalWindow');
-	let ul = document.querySelector('ul');
-	let closeButton = document.querySelector('.modalWindow .close');
-	let pencil = document.querySelector('.pencil');
 	let inputText = document.getElementById('inputText');
-
+	let ul = document.querySelector('ul');
+	// let listItem = document.querySelectorAll('.listItem');
+	// let $delete = document.querySelectorAll('.delete');
+	// let $text = document.querySelectorAll('.text');
+	// let $important = document.querySelectorAll('.important');
+	let $save = document.getElementById('save');
+	let $clear = document.getElementById('clear');
+	let tipsButton = document.getElementById('tips');
+	let pencil = document.querySelector('.pencil');
+	let modalWindow = document.querySelector('.modalWindow');
+	let closeButton = document.querySelector('.modalWindow .close');
 
 	inputText.addEventListener('keypress', function(key){
 		if(key.which !== 13){
@@ -16,33 +21,53 @@ function pageLoaded() {
 		}
 
 		let li = document.createElement('li');
-		li.setAttribute('class', 'listItem');
-		let span = document.createElement('span');
-		let span2 = document.createElement('span');
-		let span3 = document.createElement('span');
-		span3.setAttribute('class', 'important');
-		let impIcon = document.createElement('i');
-		impIcon.setAttribute('class', 'fa-solid fa-exclamation');
-		span.setAttribute('class', 'delete');
-		span2.textContent = this.value;
-		span2.setAttribute('class', 'text')
-		let icon = document.createElement('i');
-		icon.setAttribute('class', 'fa-solid fa-trash-can');
-		span.addEventListener('click', () => {
-			span.parentElement.parentElement.remove();
-		})
-		span3.addEventListener('click', () => {
-			li.style.color = 'tomato';
-		})
-		span.insertAdjacentElement('beforeend', icon);
-		span3.insertAdjacentElement('beforeend', impIcon);
-		li.insertAdjacentElement('beforeend', span);
-		li.insertAdjacentElement('beforeend', span2);
-		li.insertAdjacentElement('beforeend', span3);
+		let deleteI = document.createElement('i');
+		let text = document.createElement('span');
+		let importantI = document.createElement('i');
+
+		li.classList.add('listItem');
+		deleteI.classList.add('delete', 'fa-solid', 'fa-trash-can');
+		text.classList.add('text');
+		importantI.classList.add('important', 'fa-solid', 'fa-exclamation');
+		li.insertAdjacentElement('beforeend', deleteI);
+		text.textContent = this.value;
+		li.insertAdjacentElement('beforeend', text);
+		li.insertAdjacentElement('beforeend', importantI);
 		ul.insertAdjacentElement('beforeend', li);
 		this.value = '';
 		save();
 	});
+	loadTodos();
+	deleteTodo();
+	importantTodo();
+
+	ul.addEventListener('click', function(event){
+		if(event.target.tagName == 'LI'){
+			event.target.classList.toggle('listItemClick');
+			save();
+		}
+	});
+
+	function deleteTodo(){
+		let listItem = document.querySelectorAll('.listItem');
+		let $delete = document.querySelectorAll('.delete');
+		for(let i = 0; i < $delete.length; i++){
+			$delete[i].addEventListener('click', function(){
+				listItem[i].remove();
+				save();
+			});
+		}
+	}
+	function importantTodo(){
+		let listItem = document.querySelectorAll('.listItem');
+		let $important = document.querySelectorAll('.important');
+		for(let i = 0; i < $important.length; i++){
+			$important[i].addEventListener('click', function(){
+				listItem[i].classList.toggle('important-bg');
+			});
+			save();
+		}
+	}
 
 	pencil.addEventListener('click', () => {
 		inputText.classList.toggle('none');
@@ -53,15 +78,50 @@ function pageLoaded() {
 	closeButton.addEventListener('click', () => {
 		modalWindow.classList.remove('showModalWindow');
 	});
+	$clear.addEventListener('click',function(){
+		ul.innerHTML = '';
+		localStorage.removeItem('todos', ul.innerHTML)
+	});
+	function loadTodos(){
+		if(localStorage.getItem('todos')){
+			ul.innerHTML = localStorage.getItem('todos');
+		}
+	}
 	function save(){
 		localStorage.setItem('todos', ul.innerHTML);
 	}
-}
 
-// for(let i = 0; i < listItem.length; i++){
-// 	listItem[i].addEventListener('click', () => {
-// 		listItem[i].classList.toggle('no-decoration');
-// 	})
-// }
+	function getList(){
+		let list = localStorage.getItem('todos').split('</li>');
+		list = list.map((item)=> item + '</li>');
+		if(list[list.length-1].indexOf('<li') == -1){
+			list.pop();
+		}
+		return list;
+	}
+	$save.addEventListener('click', function() {
+		let nameBtn = ['Done', 'Active', 'Important', 'All'];
+		let index = nameBtn.indexOf(this.textContent);
+		index++;
+		if(index == nameBtn.length) index = 0;
+		this.textContent = nameBtn[index];
+		filterlist();
+	})
+	function filterlist(){
+		switch($save.textContent){
+			case 'Done':
+				ul.innerHTML = getList().filter((item)=> item.indexOf('listItemClick') !== -1).join('');
+				break;
+			case 'Active':
+				ul.innerHTML = getList().filter((item)=> item.indexOf('listItemClick') === -1).join('');
+				break;
+			case 'All':
+				ul.innerHTML = localStorage.getItem('todos');
+				break;
+			case 'Important':
+				ul.innerHTML = getList().filter((item)=> item.indexOf('important-bg') !== -1).join('');
+		}
+	}
+}
 
 window.addEventListener('DOMContentLoaded', pageLoaded);
